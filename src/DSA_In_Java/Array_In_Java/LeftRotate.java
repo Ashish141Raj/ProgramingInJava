20a9a4fd813ece7c5accede8317ff14923f9e39d
Ashish RAj <rajshish37@gmail.com>
2026-06-30 19:50:53 +0530

    initial commit

---
+++ src/DSA_In_Java/Array_In_Java/LeftRotate.java
@@ -0,0 +1,17 @@
+package DSA_In_Java.Array_In_Java;
+
+import java.util.Arrays;
+
+public class LeftRotate {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+
+        int temp = arr[0];
+        int n = arr.length;
+        for (int i = 1; i < n ; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+        System.out.println(Arrays.toString(arr));
+    }
+}

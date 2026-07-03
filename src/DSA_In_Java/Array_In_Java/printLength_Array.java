911e5ec240fc05ec5b8ea5d828d233e93de81172
Ashish RAj <rajshish37@gmail.com>
2026-07-03 21:09:37 +0530

    initial commit

---
+++ src/DSA_In_Java/Array_In_Java/printLength_Array.java
@@ -0,0 +1,18 @@
+package DSA_In_Java.Array_In_Java;
+
+public class printLength_Array {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+
+//        **without using function**
+//
+//        int count = 0;
+//        for(int i = 0; i < arr.length; i++){
+//            count ++;
+//        }
+//        System.out.println("length of Array is:  "+count);
+
+        /*With using function */
+        System.out.println(arr.length);
+    }
+}

c5a9462c7ec42fa91c5fb541981b7970f84581fb
Ashish RAj <rajshish37@gmail.com>
2026-07-07 16:32:51 +0530

    initial commit

---
+++ src/DSA_In_Java/Array_In_Java/Sorting_In_Array/InsertionSortDescendingOrder.java
@@ -0,0 +1,24 @@
+package DSA_In_Java.Array_In_Java.Sorting_In_Array;
+
+public class InsertionSortDescendingOrder {
+    public static void  DescendingOrder(int [] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] < key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        for (int num : arr){
+            System.out.print(num+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr = {12,11,13,5,6};
+        DescendingOrder(arr);
+
+    }
+}
